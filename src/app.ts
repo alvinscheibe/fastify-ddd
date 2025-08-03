d12b2040b8fd30626f4b6064e434cad67f2b5048
Alvin Scheibe <alvin.scheibe@gmail.com>
2025-08-03 10:34:42 -0300

    chore: add fastify-type-provider-zod and zod dependencies for validation

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import Fastify from 'fastify';
+import { ZodTypeProvider } from 'fastify-type-provider-zod';
 
 export const app = Fastify({
   logger: false,
-});
+}).withTypeProvider<ZodTypeProvider>();
 
 app.get('/', (req, res) => {
   res.status(200).send({ message: 'Hello World' });
