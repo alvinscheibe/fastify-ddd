a703a2223ebeb098bc285b72fb5f702fc93d7034
Alvin Scheibe <alvin.scheibe@gmail.com>
2025-08-04 14:28:37 -0300

    refactor: change Fastify imports and add custom compiler settings

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
-import Fastify from 'fastify';
-import { ZodTypeProvider } from 'fastify-type-provider-zod';
+import fastify from 'fastify';
+import { serializerCompiler, validatorCompiler, ZodTypeProvider } from 'fastify-type-provider-zod';
 import { errorHandler } from './error-handler';
 
-export const app = Fastify({
+export const app = fastify({
   logger: false,
 }).withTypeProvider<ZodTypeProvider>();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
 app.get('/', (req, res) => {
   res.status(200).send({ message: 'Hello World' });
