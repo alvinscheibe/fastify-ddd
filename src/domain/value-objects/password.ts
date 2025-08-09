821d724c3d3319ee684b6cefdcc8b2880de41bdd
Alvin Scheibe <alvin.scheibe@gmail.com>
2025-08-09 18:28:49 -0300

    refactor: password validation to use specific error class

--- src/domain/value-objects/password.ts
+++ src/domain/value-objects/password.ts
@@ -1,3 +1,5 @@
+import { InvalidPasswordError } from "@domain/errors/invalid-password-error";
+
 export class Password {
   private readonly password: string;
 
@@ -11,17 +13,17 @@ export class Password {
   }
 
   static validate(password: string) {
-    if (password.length < 8) throw new Error('Invalid password. Must be at least 8 characters long.');
+    if (password.length < 8) throw new InvalidPasswordError('Invalid password. Must be at least 8 characters long.');
 
-    if (password.includes(' ')) throw new Error('Invalid password. Must not contain spaces.');
+    if (password.includes(' ')) throw new InvalidPasswordError('Invalid password. Must not contain spaces.');
 
-    if (!/[A-Z]/.test(password)) throw new Error('Invalid password. Must contain at least one uppercase letter.');
+    if (!/[A-Z]/.test(password)) throw new InvalidPasswordError('Invalid password. Must contain at least one uppercase letter.');
 
-    if (!/[a-z]/.test(password)) throw new Error('Invalid password. Must contain at least one lowercase letter.');
+    if (!/[a-z]/.test(password)) throw new InvalidPasswordError('Invalid password. Must contain at least one lowercase letter.');
 
-    if (!/[0-9]/.test(password)) throw new Error('Invalid password. Must contain at least one number.');
+    if (!/[0-9]/.test(password)) throw new InvalidPasswordError('Invalid password. Must contain at least one number.');
 
-    if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) throw new Error('Invalid password. Must contain at least one special character.');
+    if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) throw new InvalidPasswordError('Invalid password. Must contain at least one special character.');
 
     return password;
   }
--- src/error-handler.ts
+++ src/error-handler.ts
@@ -1,3 +1,4 @@
+import { DomainError } from '@domain/errors/domain-error';
 import type { FastifyInstance } from 'fastify';
 import { hasZodFastifySchemaValidationErrors } from 'fastify-type-provider-zod';
 
@@ -6,6 +7,12 @@ type FastifyErrorHandler = FastifyInstance['errorHandler'];
 export const errorHandler: FastifyErrorHandler = async (error, _, reply) => {
   if (hasZodFastifySchemaValidationErrors(error)) {
     const errors = error.validation.map((e) => e.message);
+
+    if (error instanceof DomainError)
+      return reply.status(422).send({
+        message: 'Domain error',
+        error: error.message,
+      });
 
     return reply.status(400).send({
       message: 'Validation error',
