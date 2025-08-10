7bfe3d142f31ee958e80013ea27a17006472f31b
Alvin Scheibe <alvin.scheibe@gmail.com>
2025-08-10 16:59:36 -0300

    feat: add ApplicationError handling and improve error response format

--- src/error-handler.ts
+++ src/error-handler.ts
@@ -1,3 +1,4 @@
+import { ApplicationError } from '@application/errors/application-error';
 import { DomainError } from '@domain/errors/domain-error';
 import type { FastifyInstance } from 'fastify';
 import { hasZodFastifySchemaValidationErrors } from 'fastify-type-provider-zod';
@@ -8,17 +9,23 @@ export const errorHandler: FastifyErrorHandler = async (error, _, reply) => {
   if (hasZodFastifySchemaValidationErrors(error)) {
     const errors = error.validation.map((e) => e.message);
 
-    if (error instanceof DomainError)
-      return reply.status(422).send({
-        message: 'Domain error',
-        error: error.message,
-      });
-
     return reply.status(400).send({
       message: 'Validation error',
       errors,
     });
   }
+
+  if (error instanceof DomainError)
+    return reply.status(422).send({
+      message: error.name,
+      error: error.message,
+    });
+
+  if (error instanceof ApplicationError)
+    return reply.status(error.statusCode).send({
+      message: error.name,
+      error: error.message,
+    });
 
   return reply.status(500).send({ message: 'Internal Server Error' });
 };
